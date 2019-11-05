f355109e11425873aec70ef26ec1e14cde8e4f67
tringuyen623 <tringuyen2391@gmail.com>
2019-11-05 14:27:55 +0700

    update Bài tập Ứng dụng chuyển đổi tiền tệ

--- Biến, kiểu dữ liệu và toán tử/Bài tập ứng dụng chuyển đổi tiền tệ/convert.js
+++ Biến, kiểu dữ liệu và toán tử/Bài tập ứng dụng chuyển đổi tiền tệ/convert.js
@@ -5,13 +5,13 @@ function convert() {
     let usdTovnd = 232000, vndTousd = 0.000043 //exchange rate.
 
     if (select.value === "USD" && select1.value === "USD") {
-        document.getElementById("result").innerHTML = "Result: " + amount * 1;
-    }else if(select.value === "USD"&&select1.value ==="VND"){
-        document.getElementById("result").innerHTML="Result: "+ amount *usdTovnd;
-    }if (select.value === "VND"&& select1.value ==="VND"){
-        document.getElementById("result").innerHTML="Result: "+amount*1;
-    }else if(select.value ==="VND"&&select1.value ==="USD"){
-        document.getElementById("result").innerHTML="Result: "+amount*vndTousd;
+        document.getElementById("result").innerHTML = "Result: " + amount * 1 + ' USD';
+    } else if (select.value === "USD" && select1.value === "VND") {
+        document.getElementById("result").innerHTML = "Result: " + amount * usdTovnd +' VND';
+    } if (select.value === "VND" && select1.value === "VND") {
+        document.getElementById("result").innerHTML = "Result: " + amount * 1 + ' VND';
+    } else if (select.value === "VND" && select1.value === "USD") {
+        document.getElementById("result").innerHTML = "Result: " + amount * vndTousd + ' USD';
     }
 
 
